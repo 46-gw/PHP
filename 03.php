5d5d5f3167691cd105efc93dd6b87b89e7d6ef73
”shiro” <paralleloneday@gmail.com>
2020-11-09 07:40:48 +0000

    PHP03

---
+++ 03.php
@@ -0,0 +1,38 @@
+<?php
+
+//01
+$name = "shiro";
+if ($name == "shiro") {
+    echo "私は shiro です";
+} else {
+    echo "shiro ではありません";
+}
+
+
+//02
+$total = 0;
+echo $total;
+for ($i = 0; $i <= 10000; $i++){
+    $total += $i;
+}
+echo $total;
+
+
+//03
+$fruits = array("apple","banana","orange","grape","lemon");
+foreach($fruits as $fruits){
+    echo $fruits;
+    echo "\n";
+}
+
+
+//04
+$start = 1;
+$end = 100;
+for ($i = $start; $i <= $end; $i++){
+  // 5で割り切れたら{}内を実行する
+  if ($i % 5 == 0){
+    echo $i;
+    echo "\n";
+  }
+}
